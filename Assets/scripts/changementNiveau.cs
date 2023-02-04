66456be723ac355b27a09b8c370091315bbe1726
Stillau <joris.casin@laposte.net>
2023-02-04 21:50:09 +0100

    un peu de chgt

--- Assets/scripts/changementNiveau.cs
+++ Assets/scripts/changementNiveau.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class changementNiveau : MonoBehaviour
 {
     [SerializeField] private List<GameObject> panneau_niveau;
-    [SerializeField] private int Niveau = 0;
 
     public spawnEnnemis ewi;
     // Start is called before the first frame update
@@ -22,14 +21,14 @@ public class changementNiveau : MonoBehaviour
         {
             changer_corruption();
             spawnEnnemis.number_ennemis = 5;
-            Niveau += 1;
+            VagueHUD.Vague += 1;
             spawnEnnemis.Rentrer = false;
         }
     }
 
     void changer_corruption()
     {
-        panneau_niveau[Niveau].SetActive(false);
+        panneau_niveau[VagueHUD.Vague].SetActive(false);
     }
     
 }
