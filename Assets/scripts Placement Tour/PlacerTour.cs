139190d0f663f5928abc868a64fe0c61a699f612
Stillau <joris.casin@laposte.net>
2023-02-05 12:11:14 +0100

    tout marche à peu près bien

--- Assets/scripts Placement Tour/PlacerTour.cs
+++ Assets/scripts Placement Tour/PlacerTour.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlacerTour : MonoBehaviour
 {
     public List<GameObject> Tourelles;
-    private AllClick random;
+    [SerializeField] AllClick random;
     public List<int> thune_de_la_tour ;
 
 void Start(){
@@ -16,16 +16,16 @@ void Start(){
 void OnMouseDown()
     {
         for(int i =0; i< Tourelles.Count; i++){
-        if(random.t[i] && random.surSouris && Money.money > thune_de_la_tour[i])
-        {
-            Money.spend(thune_de_la_tour[i]);
-            Destroy (GameObject.FindWithTag("tour"));
-            GameObject tour = Instantiate(Tourelles[i],transform.parent,true);
-            tour.transform.position=transform.position;
-            random.t[i]=false;
-            random.surSouris=false;
-            this.gameObject.SetActive(false);
-        }
+            if(random.t[i] && random.surSouris && Money.money > thune_de_la_tour[i])
+            {
+                Money.spend(thune_de_la_tour[i]);
+                Destroy (GameObject.FindWithTag("tour"));
+                GameObject tour = Instantiate(Tourelles[i],transform.parent,true);
+                tour.transform.position=transform.position;
+                random.t[i]=false;
+                random.surSouris=false;
+                this.gameObject.SetActive(false);
+            }
         }
 
     }   
